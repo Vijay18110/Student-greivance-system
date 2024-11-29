877fdad469d371d414dc1ee27b6cf075dc67f228
Vijay18110 <vijay9628554907@gmail.com>
2024-11-29 20:28:49 +0530

    error solved

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../../components/Navbar'
 import Main from '../../components/Main'
 import Card from '../../components/Card'
-import styles from './Home.module.css'
+import styles from './home.module.css';
 import img1 from '../../assets/card/hands.png'
 import img2 from '../../assets/card/complaints.jpeg'
 import img3 from '../../assets/card/complaintsmain.jpeg'
