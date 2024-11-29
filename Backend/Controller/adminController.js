479db0bf8585df245b4baed5f6646f0a8f9e12d4
Vijay18110 <vijay9628554907@gmail.com>
2024-11-29 15:22:09 +0530

    error solved

--- Backend/Controller/adminController.js
+++ Backend/Controller/adminController.js
@@ -1,21 +1,15 @@
 const { adminmodel } = require("../Model/adminmodel");
-
 exports.saveadmindata = async (req, res) => {
     const model = new adminmodel(req.body);
     const data = await model.save();
-
     res.json({ msg: "registration successfull" });
 }
-
 exports.getadmindata = async (req, res) => {
     const result = await adminmodel.findOne({ id: req.body.id, password: req.body.password });
     if (result) {
-        res.json({ msg: true })
-
+        res.json({ msg: true });
     }
     else {
-        res.json({ msg: false })
-
+        res.json({ msg: false });
     }
-
 }
--- Backend/Routers/Route.js
+++ Backend/Routers/Route.js
@@ -5,20 +5,19 @@ const { saveadmindata, getadmindata } = require('../Controller/adminController')
 const { savecompaints, getcompaints, deletecom, updatecom, getsomedata } = require('../Controller/compaintsController');
 const { saveresolverdata, getresolverdata } = require('../Controller/resolverController');
 const router = express.Router();
-router.get('/server', data)
-router.post('/students', savedata)
-router.patch('/students', getdata)
-router.post('/admin', saveadmindata)
-router.patch('/admin', getadmindata)
-router.patch('/complaints', getsomedata)
-
-router.get('/students', getstudentdata)
-router.post('/complaints', savecompaints)
-router.get('/complaints', getcompaints)
-router.delete('/complaints', deletecom)
-router.post('/resolver', saveresolverdata)
-router.patch('/resolver', getresolverdata)
-router.put('/complaints', updatecom)
+router.get('/server', data);
+router.post('/students', savedata);
+router.patch('/students', getdata);
+router.post('/admin', saveadmindata);
+router.patch('/admin', getadmindata);
+router.patch('/complaints', getsomedata);
+router.get('/students', getstudentdata);
+router.post('/complaints', savecompaints);
+router.get('/complaints', getcompaints);
+router.delete('/complaints', deletecom);
+router.post('/resolver', saveresolverdata);
+router.patch('/resolver', getresolverdata);
+router.put('/complaints', updatecom);
 
 
 
